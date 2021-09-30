d49be9394cea0ca7c185575aa22a9eb6d874cfb3
John <lc@pop-os.localdomain>
2021-09-29 19:09:13 -0700

    Print token accum

--- src/transformers/sentence_detect.py
+++ src/transformers/sentence_detect.py
@@ -1,6 +1,7 @@
 from transformers import GPT2TokenizerFast
 
-ends = ['!', '.', '?', '."']
+ends = ['!', '.', '?']
+ends.extend(map(lambda end: end + '"', ends))
 
 quotation_chars = ['"', "'"]
 
@@ -18,13 +19,16 @@ default_tokenizer = GPT2TokenizerFast.from_pretrained('gpt2')
 
 def is_sentence_tokens(tokens, tokenizer=default_tokenizer):
     text: str = tokenizer.decode(tokens)
-    out = not any( #No exception ending
+    print("Token accum (sentence check): ", text)
+    out = not any(
         map(lambda ending: text.endswith(ending), exception_ends) #No non-end-of-sentence-periods
-    ) and not any(
-        map(lambda qc: text.count(qc) % 2 != 0, quotation_chars) #No unfinished quotations
     ) and not any(
         map(lambda bck: bck in text and not brace_chars[bck] in text, brace_chars) #No unfinished brace characters.
     ) and any(
         map(lambda ending: text.endswith(ending), ends),
     )
     return out
+
+if __name__ == '__main__':
+    encoding = default_tokenizer.encode("She told her she had been drunk, but she hadn")
+    print(is_sentence_tokens(encoding))
